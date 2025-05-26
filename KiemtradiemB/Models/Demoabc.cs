4744bd8f5577ad03d3a572ed8804294f56b567a5
npthuy-1110 <ngphthuy2011@gmail.com>
2025-05-26 14:33:17 +0700

    KT

--- KiemtradiemB/Models/Demoabc.cs
+++ KiemtradiemB/Models/Demoabc.cs
@@ -1,9 +1,8 @@
 namespace KiemtradiemB;
 public class Demoabc
 {
-    {
+[KEY]
     public int PesonId { get; set; }
     public string Fullname { get; set;}
     public double Diem { get ; set;}
-}
 }
